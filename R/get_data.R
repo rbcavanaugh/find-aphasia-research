97922eb3b72f443de9bba2f6ed7b696942fdce65
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-12-20 15:52:14 -0500

    testing linking

--- R/get_data.R
+++ R/get_data.R
@@ -5,7 +5,9 @@ library(tidyverse)
 library(ggmap)
 library(here)
 
-res = GET("https://clinicaltrials.gov/api/query/study_fields?expr=aphasia+AND+AREA%5BOverallStatus%5DRecruiting&fields=BriefTitle%2C+Condition%2C+StartDate%2C+NCTId%2C+OrgFullName%2C+OverallStatus%2C+BriefSummary%2C+DetailedDescription%2C+Keyword%2C+StudyType%2C+EligibilityCriteria%2C+HealthyVolunteers%2C+MinimumAge%2C+Gender%2C+LocationCity%2C+LocationZip%2C+LocationState%2C+LocationCountry&min_rnk=1&max_rnk=500&fmt=json")
+res = GET("https://clinicaltrials.gov/api/query/study_fields?expr=aphasia+AND+AREA%5BOverallStatus%5DRecruiting&fields=BriefTitle%2C+Condition%2C+StartDate%2C+NCTId%2C+OrgFullName%2C+OverallStatus%2C+BriefSummary%2C+DetailedDescription%2C+Keyword%2C+StudyType%2C+EligibilityCriteria%2C+HealthyVolunteers%2C+MinimumAge%2C+Gender%2C+LocationCity%2C+LocationZip%2C+LocationState%2C+ResponsiblePartyInvestigatorFullName%2C+LocationCountry&min_rnk=1&max_rnk=500&fmt=json")
+
+
 
 data_list = fromJSON(rawToChar(res$content))
 
@@ -25,4 +27,20 @@ data = bind_rows(data_list$StudyFieldsResponse$StudyFields) %>%
 write.csv(data, file = here("data", "clinical_trials_clean.csv"))
 
 
+res = GET("https://clinicaltrials.gov/api/query/study_fields?expr=aphasia+AND+AREA%5BOverallStatus%5DRecruiting&fields=BriefTitle%2C+Condition%2C+NCTId%2C+OrgFullName%2C+OverallStatus%2C+BriefSummary%2C+DetailedDescription%2C+Keyword%2C+StudyType%2C+EligibilityCriteria%2C+HealthyVolunteers%2C+MinimumAge%2C+Gender%2C+LocationCity%2C+LocationState%2C+ResponsiblePartyInvestigatorFullName%2C+CentralContactEmail%2C+CentralContactPhone%2C+CentralContactName%2C+LocationCountry&min_rnk=1&max_rnk=500&fmt=json")
+
+
+
+data_list = fromJSON(rawToChar(res$content))
+
+data_listcols = bind_rows(data_list$StudyFieldsResponse$StudyFields) %>% 
+  mutate(Condition = map_chr(Condition, str_c, collapse=" "),
+         Keyword = map_chr(Keyword, str_c, collapse=" ")) %>%
+  filter(OverallStatus=="Recruiting",
+         str_detect(Condition, "Progressive", negate = T),
+         str_detect(Condition, "Dementia", negate = T),
+         str_detect(Condition, "Aphasia") | str_detect(Keyword, "Aphasia")) %>%
+  filter(str_detect(LocationCountry, "United States"))
+
+write_rds(data_listcols, file = here("data", "clinical_trials_listcols.rds"))
 
--- docs/R/get_data.R
+++ docs/R/get_data.R
@@ -5,11 +5,11 @@ library(tidyverse)
 library(ggmap)
 library(here)
 
-res = GET("https://clinicaltrials.gov/api/query/study_fields?expr=aphasia+AND+AREA%5BOverallStatus%5DRecruiting&fields=BriefTitle%2C+Condition%2C+StartDate%2C+NCTId%2C+OrgFullName%2C+OverallStatus%2C+BriefSummary%2C+DetailedDescription%2C+Keyword%2C+StudyType%2C+EligibilityCriteria%2C+HealthyVolunteers%2C+MinimumAge%2C+Gender%2C+LocationCity%2C+LocationZip%2C+LocationState%2C+LocationCountry&min_rnk=1&max_rnk=500&fmt=json")
+res = GET("https://clinicaltrials.gov/api/query/study_fields?expr=aphasia+AND+AREA%5BOverallStatus%5DRecruiting&fields=BriefTitle%2C+Condition%2C+StartDate%2C+NCTId%2C+OrgFullName%2C+OverallStatus%2C+BriefSummary%2C+DetailedDescription%2C+Keyword%2C+StudyType%2C+EligibilityCriteria%2C+HealthyVolunteers%2C+MinimumAge%2C+Gender%2C+LocationCity%2C+LocationZip%2C+LocationState%2C+ResponsiblePartyInvestigatorFullName%2C+LocationCountry&min_rnk=1&max_rnk=500&fmt=json")
+
+
 
 data_list = fromJSON(rawToChar(res$content))
-
-#register_google(key="AIzaSyBhAhndR8Ghki9qb-Wc2DLh82iGFzzAgDo")
 
 data = bind_rows(data_list$StudyFieldsResponse$StudyFields) %>% 
   mutate(Condition = map_chr(Condition, str_c, collapse=" "),
@@ -27,4 +27,20 @@ data = bind_rows(data_list$StudyFieldsResponse$StudyFields) %>%
 write.csv(data, file = here("data", "clinical_trials_clean.csv"))
 
 
+res = GET("https://clinicaltrials.gov/api/query/study_fields?expr=aphasia+AND+AREA%5BOverallStatus%5DRecruiting&fields=BriefTitle%2C+Condition%2C+NCTId%2C+OrgFullName%2C+OverallStatus%2C+BriefSummary%2C+DetailedDescription%2C+Keyword%2C+StudyType%2C+EligibilityCriteria%2C+HealthyVolunteers%2C+MinimumAge%2C+Gender%2C+LocationCity%2C+LocationState%2C+ResponsiblePartyInvestigatorFullName%2C+CentralContactEmail%2C+CentralContactPhone%2C+CentralContactName%2C+LocationCountry&min_rnk=1&max_rnk=500&fmt=json")
+
+
+
+data_list = fromJSON(rawToChar(res$content))
+
+data_listcols = bind_rows(data_list$StudyFieldsResponse$StudyFields) %>% 
+  mutate(Condition = map_chr(Condition, str_c, collapse=" "),
+         Keyword = map_chr(Keyword, str_c, collapse=" ")) %>%
+  filter(OverallStatus=="Recruiting",
+         str_detect(Condition, "Progressive", negate = T),
+         str_detect(Condition, "Dementia", negate = T),
+         str_detect(Condition, "Aphasia") | str_detect(Keyword, "Aphasia")) %>%
+  filter(str_detect(LocationCountry, "United States"))
+
+write_rds(data_listcols, file = here("data", "clinical_trials_listcols.rds"))
 
