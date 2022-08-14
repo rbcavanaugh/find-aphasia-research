df99d8175426f6cc1d30ccbcafa753632a505111
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2022-08-13 20:08:17 -0400

    switch to emojifont

--- docs/R/get_data.R
+++ docs/R/get_data.R
@@ -1,22 +1,26 @@
 
-
 library(httr)
 library(jsonlite)
 library(tidyverse)
 library(here)
-library(mapboxapi)
+
 
 # read in manual data
 manual = read_csv(here("data", "manual-submissions.csv")) %>%
   mutate(clinicaltrialsgov = 0)
 
+print("Read data")
 
 res = GET("https://clinicaltrials.gov/api/query/study_fields?expr=aphasia+AND+AREA%5BOverallStatus%5DRecruiting&fields=BriefTitle%2C+Condition%2C+StartDate%2C+NCTId%2C+OrgFullName%2C+OverallStatus%2C+BriefSummary%2C+DetailedDescription%2C+Keyword%2C+StudyType%2C+EligibilityCriteria%2C+HealthyVolunteers%2C+MinimumAge%2C+Gender%2C+LocationCity%2C+LocationZip%2C+LocationState%2C+ResponsiblePartyInvestigatorFullName%2C+LocationCountry&min_rnk=1&max_rnk=500&fmt=json")
+
+print("pulled from ct.gov")
 
 #source(here("R", "mapbox.R"))
 #mb_access_token(token, install = TRUE)
 #mb_geocode("Pittsburgh Pennsylvania United States")
 data_list = fromJSON(rawToChar(res$content))
+
+print("converted to json")
 
 data = bind_rows(data_list$StudyFieldsResponse$StudyFields) %>% 
   mutate(Condition = map_chr(Condition, str_c, collapse=" "),
@@ -29,27 +33,37 @@ data = bind_rows(data_list$StudyFieldsResponse$StudyFields) %>%
   #filter(str_detect(LocationCountry, "United States")) %>%
   unite(remove = F, location, OrgFullName, LocationCity, LocationState, LocationCountry, sep = " ") %>%
   rowwise() %>%
-  mutate(clinicaltrialsgov=1,
-         Remote = "no")
+  mutate(clinicaltrialsgov=1, Remote = "no")
+
+print("cleaned data")
 
 add_manual = manual %>%
   select(NCTId, BriefTitle, location, ResponsiblePartyInvestigatorFullName, StartDate, OrgFullName, LocationCity, LocationState, Remote)
 
+print("added manual")
+
+data2 = bind_rows(data, add_manual)
+
+print("bind rows")
+
 data2 = bind_rows(data, add_manual) %>%
-  mutate(geo = list(mb_geocode(location)),
-                lon = geo[1],
-                lat = geo[2],
+  mutate(geo = list(mb_geocode(location, access_token = token)),
+         lon = geo[1],
+         lat = geo[2],
          date=lubridate::mdy(StartDate)) %>%
   select(-geo)
 
+print("did mapbox stuff successfully")
 
 
 write.csv(data2, file = here("data", "clinical_trials_clean.csv"))
-
+print("Saved data")
 
 res = GET("https://clinicaltrials.gov/api/query/study_fields?expr=aphasia+AND+AREA%5BOverallStatus%5DRecruiting&fields=BriefTitle%2C+Condition%2C+NCTId%2C+OrgFullName%2C+OverallStatus%2C+BriefSummary%2C+DetailedDescription%2C+Keyword%2C+StudyType%2C+EligibilityCriteria%2C+HealthyVolunteers%2C+MinimumAge%2C+Gender%2C+LocationCity%2C+LocationState%2C+ResponsiblePartyInvestigatorFullName%2C+CentralContactEmail%2C+CentralContactPhone%2C+CentralContactName%2C+LocationCountry&min_rnk=1&max_rnk=500&fmt=json")
 
 data_list = fromJSON(rawToChar(res$content), flatten = T, simplifyVector =T)
+
+print("v2 retrieved")
 
 data_listcols = bind_rows(data_list$StudyFieldsResponse$StudyFields) %>% 
   mutate(Condition = map_chr(Condition, str_c, collapse=" "),
@@ -70,6 +84,9 @@ contact$clinicaltrialsgov = 1
 
 contact <- bind_rows(contact, manual[,c("NCTId", "CentralContactEMail", "CentralContactPhone", "CentralContactName", "clinicaltrialsgov")])
   
+
+print("v2 cleaned")
+
 location <- data_listcols %>%
   select(starts_with("Location"), NCTId) %>%
   unnest(everything(),  keep_empty = T) %>%
@@ -105,3 +122,4 @@ data_DT = data2 %>%
 
 write_rds(data_DT, file = here("data", "data_dt.rds"))
 
+print("v2 written")
--- docs/R/update_website.R
+++ docs/R/update_website.R
@@ -1,4 +1,10 @@
-setwd("~/github-repos/find-aphasia-research")
+#setwd("~/github-repos/find-aphasia-research")
+
+library(mapboxapi)
+# Comment out for local push
+token = Sys.getenv("MAPBOX")
+print(nchar(token))
+#mb_access_token(token, install = FALSE)
 
 library(here)
 
@@ -6,9 +12,10 @@ here()
 
 source(here("R", "get_data.R"))
 
+print("get data successful!!")
+
 Sys.sleep(5)
 
 Sys.setenv(RSTUDIO_PANDOC="/Applications/RStudio.app/Contents/MacOS/pandoc")
 
 rmarkdown::render_site(encoding = 'UTF-8')
-
